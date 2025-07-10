4915931c6db8b2fab012e51be4e77786b389495f
Snusii <Snusii1995@gmail.com>
2025-07-10 19:39:39 +0200

    Made 2 SVG files. Structured a bit more on main content.

--- app/page.jsx
+++ app/page.jsx
@@ -6,15 +6,78 @@ export default function Home() {
     
     <>
     <section>
-      <h1>FAST, FAIR AND AUTOMATED</h1>
-      <p>TapIn is your go-to hub for hosting gaming events directly in Discord</p>
-    </section>
-
-    <section>
       
+      <div>
+      <h1 className='blue-text'>FAST, FAIR AND AUTOMATED</h1>
+
+      <p>TapIn is your go-to hub for hosting gaming events directly in Discord</p>
+      </div>
+
+      <div>
+        <img src="/Images/joinnow.png" alt="Join now image with Discord link"/>
+
+        <span>
+          <img src="/Images/purplecircle.svg" alt="Purple circle icon"/>
+
+          <ul>
+            BOT
+            <li>- Setting up events.</li>
+          </ul>
+        </span>
+
+        <span>
+          <img src="/Images/bluecircle.svg" alt="Blue circle icon"/>
+
+          <ul>
+            TapIn
+            <li>
+              - React to events on Discord.
+            </li>
+            <li>
+              - Join the fun!
+            </li>
+          </ul>
+        </span>
+
+        <span>
+          <img src="/Images/purplecircle.svg" alt="Purple circle icon"/>
+
+          <ul>
+            Leaderboard
+            <li>- Updates after each event.</li>
+          </ul>
+        </span>
+
+      </div>
+
+    </section>
+
+   <section>
+
+      <h2>Why TapIn?</h2>
+
+      <ul className="blue-text">
+        Efficient and automated
+        <li>No need to manage signups or create teams manually.</li>
+      </ul>
+
+      <ul className="blue-text">
+        Balanced competition
+        <li>Fair teams based on performance.</li>
+      </ul>
+
+      <ul className="blue-text">
+        Engaging and social
+        <li>Seamless voice setup and dynamic gameplay.</li>
+      </ul>
+
     </section>
 
     <section>
+
+       <h2>Ready to Tap In?</h2>
+
+       <p>Join Discord to connect with the community, and hit Leaderboard to see today's top players!</p>
        
     </section>
 </>
